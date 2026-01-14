174302bc742a613424c07fddc54f7e698c09036d
tessavermeulen07 <tessavermeulen@me.com>
2026-01-14 11:47:50 +0100

    opdracht 1 t/m 3 klaar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,61 @@
 import './App.css'
+import axios from "axios";
+import {useEffect, useState} from "react";
+import pokemonCard from "./components/PokemonCard.jsx";
+import PokemonCard from "./components/PokemonCard.jsx";
+
 
 function App() {
 
-  return (
-    <>
-      <h1>Gotta catch em all!</h1>
-    </>
-  )
+    const [pokemon, setPokemon] = useState(null);
+
+    async function fetchPokemon() {
+        try {
+            const result = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+            console.log(result);
+            setPokemon(result.data);
+        } catch (error) {
+            `Het laden van de Pokemon is niet gelukt`
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        void fetchPokemon();
+    }, []);
+
+    return (
+        <>
+            <h1>Gotta catch em all!</h1>
+
+            {/*<ul className="list-container">*/}
+            {/*    {blogposts.map((blog) => {*/}
+            {/*            return <li key={blog.id}>*/}
+            {/*                <article className="list-item-container">*/}
+            {/*                    <span><Link to={`/blog/${blog.id}`}*/}
+            {/*                                element={<SingleBlog/>}>{blog.title}</Link> ({blog.author})</span>*/}
+            {/*                    <span>{blog.comments} reacties - {blog.shares} keer gedeeld</span>*/}
+            {/*                </article>*/}
+            {/*            </li>*/}
+            {/*        }*/}
+            {/*    )}*/}
+            {/*</ul>*/}
+
+            <ul>
+                {pokemon?.results?.length > 0 &&
+                    (pokemon?.results?.map((pokemon) => {
+                        return <li key={pokemon.name}>
+                            <PokemonCard
+                                url={pokemon.url}
+                            />
+
+                        </li>
+
+                    }))
+                }
+            </ul>
+        </>
+    )
 }
 
 export default App
---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,45 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+function PokemonCard({url}) {
+
+    const [pokemon, setPokemon] = useState({});
+
+    async function singlePokemon() {
+        try {
+            const result = await axios.get(url);
+            console.log(result);
+            setPokemon(result.data);
+        } catch (error) {
+            `Het laden van de Pokemon is niet gelukt`
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        void singlePokemon();
+    }, []);
+
+    return (
+        <>
+            {Object.keys(pokemon).length > 0 &&
+                <article className="pokemon-card">
+                    <span>{pokemon.name}</span>
+                    <span><img src={pokemon.sprites.front_default} alt={pokemon.name} /></span>
+                    <span>Moves: {pokemon.moves.length}</span>
+                    <span>Weight: {pokemon.weight}</span>
+                    <span>Abilities:</span>
+                    <ul>
+                        {pokemon.abilities.map((ability) => (
+                            <li key={ability.ability.name}>
+                                {ability.ability.name}
+                            </li>
+                        ))}
+                    </ul>
+                </article>
+            }
+        </>
+    )
+}
+
+export default PokemonCard;
